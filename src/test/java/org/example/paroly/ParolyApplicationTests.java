505c777acb76bb507f4e479deddf08be85a3ee0c
MesVortex <meskinemostafa4@gmail.com>
2024-12-09 16:32:12 +0100

    Intialize project's structure

---
+++ src/test/java/org/example/paroly/ParolyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.paroly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParolyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
